1d37ab799eeb8efb061fd299d54843f0fd99a7f9
cyberace313 <zafer.arifi290@gmail.com>
2024-01-31 15:10:06 +0430

    Add javascript functionality for mobile view navigation

---
+++ script.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".mobile-menu");
+const close = document.querySelector(".close-icon");
+const body = document.querySelector(".body");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("nav-visible");
+  close.style.display = "block";
+  menu.style.display = "none";
+  body.classList.add("body-no-scroll");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("nav-visible");
+  close.style.display = "none";
+  menu.style.display = "block";
+  body.classList.remove("body-no-scroll");
+});
